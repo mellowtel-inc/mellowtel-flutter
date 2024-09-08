96d75a899e96acd05329b36f052987a31b49d3dc
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-09-08 15:32:11 +0530

    (feat): rename extension to app

--- lib/src/ui/consent_settings_dailog.dart
+++ lib/src/ui/consent_settings_dailog.dart
@@ -22,7 +22,7 @@ class ConsentSettingsDialog extends StatefulWidget {
     required this.initiallyOptedIn,
     required this.onOptIn,
     required this.onOptOut,
-  }): super(key: key);
+  }) : super(key: key);
 
   @override
   ConsentSettingsDialogState createState() => ConsentSettingsDialogState();
@@ -150,11 +150,11 @@ class ConsentSettingsDialogState extends State<ConsentSettingsDialog> {
   String _getInformationText() {
     switch (_state) {
       case ContainerState.optedIn:
-        return "Mellowtel is an open-source library that lets you share your unused internet with trusted Al labs & startups who use it to train their models. The developer of this extension gets a small share of the revenue. It helps maintain this extension free and available. Mellowtel shares your bandwidth only. Security and privacy are 100% guaranteed. It doesn't collect, share, or sell personal information (not even anonymized data).";
+        return "Mellowtel is an open-source library that lets you share your unused internet with trusted Al labs & startups who use it to train their models. The developer of this app gets a small share of the revenue. It helps maintain this app free and available. Mellowtel shares your bandwidth only. Security and privacy are 100% guaranteed. It doesn't collect, share, or sell personal information (not even anonymized data).";
       case ContainerState.optedOut:
         return 'You are currently opted out. Your device\'s resources are not being used.';
       case ContainerState.confirmOptOut:
-        return 'Mellowtel is used by hundreds of thousands of users around the world. By remaining opted in, you will join this growing network of users. Security, privacy and speed of browsing are 100% guaranteed.\n\nOpting-out might negatively affect the quality of the service offered by this extension. Please consider opting-in to help keep this extension free and available.';
+        return 'Mellowtel is used by hundreds of thousands of users around the world. By remaining opted in, you will join this growing network of users. Security, privacy and speed of browsing are 100% guaranteed.\n\nOpting-out might negatively affect the quality of the service offered by this app. Please consider opting-in to help keep this app free and available.';
     }
   }
 
@@ -219,8 +219,8 @@ class ConsentSettingsDialogState extends State<ConsentSettingsDialog> {
                   });
                 },
                 style: ElevatedButton.styleFrom(
-                  onSurface: Theme.of(context).primaryColor,
-                  primary: Theme.of(context).colorScheme.onPrimary,
+                  primary: Theme.of(context).primaryColor,
+                  onPrimary: Theme.of(context).colorScheme.onPrimary,
                   padding: const EdgeInsets.symmetric(
                     horizontal: 16.0,
                     vertical: 16.0,
@@ -253,12 +253,12 @@ class ConsentSettingsDialogState extends State<ConsentSettingsDialog> {
               child: ElevatedButton(
                 onPressed: () async {
                   await widget.onOptIn();
-                 
+
                   Navigator.pop(context);
                 },
                 style: ElevatedButton.styleFrom(
-                  onSurface: Theme.of(context).primaryColor,
-                  primary: Theme.of(context).colorScheme.onPrimary,
+                  primary: Theme.of(context).primaryColor,
+                  onPrimary: Theme.of(context).colorScheme.onPrimary,
                   padding: const EdgeInsets.symmetric(
                     horizontal: 16.0,
                     vertical: 16.0,
@@ -294,8 +294,8 @@ class ConsentSettingsDialogState extends State<ConsentSettingsDialog> {
                   Navigator.pop(context);
                 },
                 style: ElevatedButton.styleFrom(
-                  onSurface: Theme.of(context).primaryColor,
-                  primary: Theme.of(context).colorScheme.onPrimary,
+                  primary: Theme.of(context).primaryColor,
+                  onPrimary: Theme.of(context).colorScheme.onPrimary,
                   padding: const EdgeInsets.symmetric(
                     horizontal: 16.0,
                     vertical: 16.0,
