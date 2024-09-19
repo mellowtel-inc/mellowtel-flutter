92790dfcb4208bee3d4bf4ee3b0ceed82ef90017
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-09-19 22:49:44 +0530

    typed data import

--- lib/src/webview/macos_webview_manager.dart
+++ lib/src/webview/macos_webview_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer' as developer;
+import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:mellowtel/mellowtel.dart';
